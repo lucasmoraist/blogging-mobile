da189ca6fbf1f2906aa6f0165891250233bd81c7
lucasmoraist <luksmnt1101@gmail.com>
2024-11-26 16:43:08 -0300

    feat: add mock data for posts, teachers, and users

---
+++ src/mocks/teacher.ts
@@ -0,0 +1,44 @@
+import { ITeacher } from "../interface/teacher.interface";
+
+export const TeacherMock: ITeacher[] = [
+    {
+        name: 'Ana Silva',
+        school_subject: 'biologia',
+        user_id: 1
+    },
+    {
+        name: 'Carlos Souza',
+        school_subject: 'fisica',
+        user_id: 2
+    },
+    {
+        name: 'Maria Costa',
+        school_subject: 'geografia',
+        user_id: 3
+    },
+    {
+        name: 'Paulo Lima',
+        school_subject: 'historia',
+        user_id: 4
+    },
+    {
+        name: 'Joana Alves',
+        school_subject: 'ingles',
+        user_id: 5
+    },
+    {
+        name: 'Rafael Braga',
+        school_subject: 'matematica',
+        user_id: 6
+    },
+    {
+        name: 'Lucia Mota',
+        school_subject: 'portugues',
+        user_id: 7
+    },
+    {
+        name: 'Marcos Pereira',
+        school_subject: 'quimica',
+        user_id: 8
+    },
+]
