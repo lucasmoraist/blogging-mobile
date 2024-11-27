70a3929c95caa85e6d3fe3fd290b17790f322341
lucasmoraist <luksmnt1101@gmail.com>
2024-11-26 22:06:36 -0300

    feat: created post-details screen

---
+++ src/screens/post-details/index.tsx
@@ -0,0 +1,130 @@
+import {useEffect, useState} from 'react';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {IPost} from '../../interface/post.interface';
+import {getOnePost} from '../../service/post/getOnePost';
+
+export function PostDetails() {
+  const id = 1;
+
+  const [post, setPost] = useState<IPost>();
+
+  useEffect(() => {
+    const response = getOnePost(id);
+    setPost(response);
+  }, []);
+
+  const formatDate = (date: string) => {
+    const dateObj = new Date(date);
+    return dateObj.toLocaleDateString('pt-br', {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+    });
+  };
+
+  if (!post) {
+    return <Text>Loading...</Text>;
+  }
+
+  return (
+    <ScrollView style={styles.container}>
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnText}>Voltar</Text>
+      </TouchableOpacity>
+
+      <View style={styles.about}>
+        <View>
+          <Image source={{uri: post.url_image}} style={styles.image} />
+          <Text style={styles.title}>{post.title}</Text>
+        </View>
+
+        <View style={styles.info}>
+          <View style={styles.author}>
+            <Text style={styles.name}>{post.teacher.name}</Text>
+            <Text style={{fontSize: 16, fontWeight: 'bold'}}>•</Text>
+            <Text style={styles.school_subject}>
+              {post.teacher.school_subject}
+            </Text>
+          </View>
+          <Text style={styles.created_at}>{formatDate(post.created_at)}</Text>
+        </View>
+      </View>
+      <Text style={styles.content}>{post.content}</Text>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 20,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+  },
+  btn: {
+    backgroundColor: '#e36414',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#fff',
+    width: 60,
+    height: 30,
+    borderRadius: 30,
+    marginBottom: 10,
+  },
+  btnText: {
+    color: '#fff',
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  about: {
+    gap: 10,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    borderRadius: 5,
+    marginBottom: 8,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    color: '#219ebc',
+  },
+  info: {
+    gap: 5,
+  },
+  author: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: 200,
+    gap: 10,
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 24,
+  },
+  school_subject: {
+    fontSize: 18,
+    lineHeight: 24,
+  },
+  created_at: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#6c757d',
+  },
+  content: {
+    fontSize: 16,
+    lineHeight: 24,
+    textAlign: 'justify',
+    marginVertical: 15,
+  },
+});
